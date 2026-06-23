610f263f0715ca38ba2df1fca6bc394cfae724cd
Clancì <60186351+Clancini@users.noreply.github.com>
2026-06-23 20:30:08 +0200

    Thoughts filtering
    + tests for it

---
+++ Dialuverc.Editor.Tests/Deductions/ThoughtsFilterTests.cs
@@ -0,0 +1,117 @@
+﻿using Dialuverc.Deductions;
+using Dialuverc.Editor.Deductions;
+
+namespace Dialuverc.Editor.Tests.Deductions
+{
+    internal class ThoughtsFilterTests
+    {
+        ThoughtsFilter _filter;
+
+        Thought _tychonThought = new Thought(Guid.NewGuid(), "nameKeyOne", "descriptionKeyOne", CharacterSides.Tychon);
+        Thought _forgerThought = new Thought(Guid.NewGuid(), "nameKeyTwo", "descriptionKeyTwo", CharacterSides.Forger);
+        Thought _anyThought = new Thought(Guid.NewGuid(), "nameKeyThree", "descriptionKeyThree", CharacterSides.Any);
+
+        [SetUp]
+        public void SetUp()
+        {
+            _filter = new ThoughtsFilter();
+        }
+
+        [Test]
+        public void AllPassWithNoFilter()
+        {
+            Assert.That(_filter.PassesAll(_tychonThought), Is.True);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.True);
+            Assert.That(_filter.PassesAll(_anyThought), Is.True);
+        }
+
+        [Test]
+        public void AnySidePassesNotStrict()
+        {
+            _filter.SideFilter = CharacterSides.Tychon;
+
+            Assert.That(_filter.PassesAll(_tychonThought), Is.True);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.False);
+            Assert.That(_filter.PassesAll(_anyThought), Is.True);
+
+            _filter.SideFilter = CharacterSides.Forger;
+
+            Assert.That(_filter.PassesAll(_tychonThought), Is.False);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.True);
+            Assert.That(_filter.PassesAll(_anyThought), Is.True);
+        }
+
+        [Test]
+        public void AnySideDoesNotPassStrict()
+        {
+            _filter.Strict = true;
+
+            _filter.SideFilter = CharacterSides.Tychon;
+
+            Assert.That(_filter.PassesAll(_tychonThought), Is.True);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.False);
+            Assert.That(_filter.PassesAll(_anyThought), Is.False);
+
+            _filter.SideFilter = CharacterSides.Forger;
+
+            Assert.That(_filter.PassesAll(_tychonThought), Is.False);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.True);
+            Assert.That(_filter.PassesAll(_anyThought), Is.False);
+
+            _filter.SideFilter = CharacterSides.Any;
+
+            Assert.That(_filter.PassesAll(_tychonThought), Is.False);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.False);
+            Assert.That(_filter.PassesAll(_anyThought), Is.True);
+        }
+
+        [Test]
+        public void AllPassWithBlankOrNullString()
+        {
+            _filter.NameKeyFilter = "       ";
+
+            Assert.That(_filter.PassesAll(_tychonThought), Is.True);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.True);
+            Assert.That(_filter.PassesAll(_anyThought), Is.True);
+
+            _filter.NameKeyFilter = null;
+
+            Assert.That(_filter.PassesAll(_tychonThought), Is.True);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.True);
+            Assert.That(_filter.PassesAll(_anyThought), Is.True);
+        }
+
+        [Test]
+        public void StringIsCaseInsensitive()
+        {
+            Assert.That(_forgerThought.NameKey.All(c => char.IsUpper(c)), Is.False);
+
+            _filter.NameKeyFilter = _forgerThought.NameKey.ToUpper();
+
+            Assert.That(_filter.PassesAll(_tychonThought), Is.False);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.True);
+            Assert.That(_filter.PassesAll(_anyThought), Is.False);
+        }
+
+        [Test]
+        public void PartialStringMatchPasses()
+        {
+            _filter.NameKeyFilter = "One";
+
+            Assert.That(_filter.PassesAll(_tychonThought), Is.True);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.False);
+            Assert.That(_filter.PassesAll(_anyThought), Is.False);
+        }
+
+        [Test]
+        public void NoPassFilterCombo()
+        {
+            _filter.NameKeyFilter = "Two";
+            _filter.SideFilter = CharacterSides.Tychon;
+
+            Assert.That(_filter.PassesAll(_tychonThought), Is.False);
+            Assert.That(_filter.PassesAll(_forgerThought), Is.False);
+            Assert.That(_filter.PassesAll(_anyThought), Is.False);
+        }
+    }
+}
---
+++ Dialuverc.Editor/Deductions/ThoughtsFilter.cs
@@ -0,0 +1,51 @@
+﻿using Dialuverc.Deductions;
+
+namespace Dialuverc.Editor.Deductions
+{
+    /// <summary>
+    /// Defines a set of criteria against which a <see cref="Thought"/> can be tested.
+    /// </summary>
+    public class ThoughtsFilter
+    {
+        public CharacterSides SideFilter { get; set; } = CharacterSides.Any;
+
+        /// <summary>
+        /// An always-case-insensitive string that must be contained in <see cref="Thought.NameKey"/> to pass.
+        /// </summary>
+        public string NameKeyFilter { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Whether passing the filter requires an exact <see cref="CharacterSides"/> match.
+        /// <para>When <see langword="false"/>, <see cref="CharacterSides.Any"/> passes for all sides.</para>
+        /// </summary>
+        public bool Strict { get; set; } = false;
+
+        bool PassesSidesFilter(CharacterSides side)
+        {
+            if (SideFilter == side)
+                return true;
+
+            if (!Strict && 
+                (SideFilter == CharacterSides.Any || side == CharacterSides.Any))
+                return true;
+
+            return false;
+        }
+
+        bool PassesNameKeyFilter(string nameKey)
+        {
+            if (nameKey is null)
+                throw new ArgumentNullException(nameof(nameKey), $"A {nameof(Thought)} can't have a null name key");
+
+            if (string.IsNullOrWhiteSpace(NameKeyFilter))
+                return true;
+
+            return nameKey.Contains(NameKeyFilter, StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// Whether the <paramref name="thought"/> passes all current filters.
+        /// </summary>
+        public bool PassesAll(Thought thought) => PassesSidesFilter(thought.Side) && PassesNameKeyFilter(thought.NameKey);
+    }
+}
