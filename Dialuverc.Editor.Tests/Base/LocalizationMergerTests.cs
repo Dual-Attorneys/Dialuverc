d16a18ad744ed728117fbab974069d1985cf4c5c
Clancì <60186351+Clancini@users.noreply.github.com>
2026-09-23 22:03:39 +0200

    Initial implementation of Flat-File localization merger (#5)
    Updates or creates a
    [flat-file](https://en.wikipedia.org/wiki/Flat-file_database) containing
    localizable keys (and possibly values).
    
    The existing content is merged with the passed collection of keys with
    the following rules based on where the key is present:
    | Is in collection | Is in file | Result |
    |---|---|---|
    | ✔️ | ✔️ | Line is copied |
    | ✔️ | ❌ | New line with key is appended at end of file |
    | ❌ | ✔️ | Line is lost |
    
    Column names are always set to LocalizationMergerFlatFile.ColumnNames if
    the file doesn't exist yet.
    If it does, they're copied without any special processing on the
    corresponding values.
    
    **Note**: Appending is unordered.
    
    More information: [delimiter-separated values
    formats](https://en.wikipedia.org/wiki/Delimiter-separated_values).

---
+++ Dialuverc.Editor.Tests/Base/LocalizationMergerTests.cs
@@ -0,0 +1,105 @@
+using Dialuverc.Editor.Base.IO;
+using Dialuverc.Editor.Base.Localization;
+
+namespace Dialuverc.Editor.Tests.Base
+{
+    internal class LocalizationMergerTests
+    {
+        string _tempStoragePathForTest => $"Dialuverc{nameof(LocalizationMergerTests)}{DateTimeOffset.UtcNow.ToUnixTimeMilliseconds()}";
+
+        LocalizationMergerFlatFile _merger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _merger = new LocalizationMergerFlatFile(new string[] { "key", "value" });
+        }
+
+        [Test]
+        public void FileIsCreatedIfNotExisting()
+        {
+            HashSet<string> keys = new HashSet<string>() { "key1", "key2", "key3",  "key4" };
+
+            using (TemporaryFileStorage tempStorage = new TemporaryFileStorage(
+                Path.Combine(Path.GetTempPath(), _tempStoragePathForTest)))
+            {
+                string outputPath = Path.Combine(tempStorage.FolderPath, "localizationOut.csv");
+
+                Assert.That(File.Exists(outputPath), Is.False);
+
+                _merger.Merge(keys, outputPath, LocalizationMergerFlatFile.CSVSeparator);
+
+                Assert.That(File.Exists(outputPath), Is.True);
+
+                string[] fileLines = File.ReadAllLines(outputPath);
+
+                // Asserts have to ignore the first line which contains column names.
+                Assert.That(keys.Count, Is.EqualTo(fileLines.Length - 1));
+
+                Assert.That(fileLines[0], Is.EqualTo(string.Join(LocalizationMergerFlatFile.CSVSeparator, _merger.ColumnNames)));
+
+                foreach (string line in fileLines.Skip(1))
+                {
+                    Assert.That(keys.Contains(line));
+                }
+            }
+        }
+
+        [Test]
+        public void KeysAreAddedIfNotInFile()
+        {
+            HashSet<string> keys = new HashSet<string>() { "key1", "key2", "key3",  "key4" };
+
+            string startingFileContent =
+@$"key{LocalizationMergerFlatFile.CSVSeparator}value
+key2{LocalizationMergerFlatFile.CSVSeparator}value2
+key3{LocalizationMergerFlatFile.CSVSeparator}value3{Environment.NewLine}";
+
+            string expectedFinalFileContent =
+@$"key{LocalizationMergerFlatFile.CSVSeparator}value
+key2{LocalizationMergerFlatFile.CSVSeparator}value2
+key3{LocalizationMergerFlatFile.CSVSeparator}value3
+key1
+key4{Environment.NewLine}";
+
+            WriteAndAssertTextsAreEqual(keys, startingFileContent, expectedFinalFileContent);
+        }
+
+        [Test]
+        public void KeysAreRemovedIfNotInCollection()
+        {
+            HashSet<string> keys = new HashSet<string>() { "key2", "key3" };
+
+            string startingFileContent =
+@$"key{LocalizationMergerFlatFile.CSVSeparator}value
+key1{LocalizationMergerFlatFile.CSVSeparator}value1
+key2{LocalizationMergerFlatFile.CSVSeparator}value2
+key3{LocalizationMergerFlatFile.CSVSeparator}value3
+key4{LocalizationMergerFlatFile.CSVSeparator}value4{Environment.NewLine}";
+
+            string expectedFinalFileContent =
+@$"key{LocalizationMergerFlatFile.CSVSeparator}value
+key2{LocalizationMergerFlatFile.CSVSeparator}value2
+key3{LocalizationMergerFlatFile.CSVSeparator}value3{Environment.NewLine}";
+
+            WriteAndAssertTextsAreEqual(keys, startingFileContent, expectedFinalFileContent);
+        }
+
+        void WriteAndAssertTextsAreEqual(IReadOnlySet<string> keys, string starting, string expected)
+        {
+            using (TemporaryFileStorage tempStorage = new TemporaryFileStorage(
+                Path.Combine(Path.GetTempPath(), _tempStoragePathForTest)))
+            {
+                string outputPath = Path.Combine(tempStorage.FolderPath, "localizationOut.csv");
+
+                File.WriteAllText(outputPath, starting);
+
+                _merger.Merge(keys, outputPath, LocalizationMergerFlatFile.CSVSeparator);
+
+                string finalFileContent = File.ReadAllText(outputPath);
+
+                Assert.That(finalFileContent, Is.EqualTo(expected));
+            }
+        }
+    }
+}
---
+++ Dialuverc.Editor/Base/Localization/LocalizationMergerFlatFile.cs
@@ -0,0 +1,131 @@
+namespace Dialuverc.Editor.Base.Localization
+{
+    public class LocalizationMergerFlatFile
+    {
+        public const char CSVSeparator = ',';
+        public const char TSVSeparator = '\t';
+
+        public readonly ICollection<string> ColumnNames;
+
+        public LocalizationMergerFlatFile(ICollection<string> columnNames)
+        {
+            ArgumentNullException.ThrowIfNull(columnNames);
+
+            ColumnNames = columnNames;
+        }
+
+        /// <summary>
+        /// Updates or creates a <see href="https://en.wikipedia.org/wiki/Flat-file_database">flat-file</see> containing localizable keys (and possibly values).
+        /// <para>
+        /// The existing content is merged with the passed <paramref name="allExistingKeys"/> with the following rules based on where the key is present:<br/>
+        /// <list type="table">
+        /// <item>
+        /// <term>In collection</term>
+        /// <term>In file</term>
+        /// <description>Result</description>
+        /// </item>
+        /// <item>
+        /// <term>Y</term>
+        /// <term>Y</term>
+        /// <description>Line is copied</description>
+        /// </item>
+        /// <item>
+        /// <term>Y</term>
+        /// <term>N</term>
+        /// <description>New line with key is appended at end of file</description>
+        /// </item>
+        /// <item>
+        /// <term>N</term>
+        /// <term>Y</term>
+        /// <description>Line is lost</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// <para>
+        /// Column names are always set to <see cref="ColumnNames"/> if the file doesn't exist yet.<br/>
+        /// If it does, they're copied without any special processing on the corresponding values.
+        /// </para>
+        /// <para>
+        /// <b>Note</b>: Appending is unordered.
+        /// </para>
+        /// </summary>
+        /// <param name="allExistingKeys">Determines which keys currently exist.</param>
+        /// <param name="filePath"></param>
+        /// <param name="separator"></param>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        public void Merge(IReadOnlySet<string> allExistingKeys, string filePath, char separator)
+        {
+            ReadOnlySpan<char> fileName = Path.GetFileNameWithoutExtension(filePath);
+
+            string? directoryPath = Path.GetDirectoryName(filePath);
+
+            if (string.IsNullOrWhiteSpace(directoryPath))
+                throw new DirectoryNotFoundException($"Can't merge localization because folder containing the file at path '{filePath}' couldn't be retrieved");
+
+            string tempFilePath = Path.Combine(directoryPath, $"_{fileName}_{Guid.NewGuid()}");
+
+            // TODO: This doesn't account for ordering, should it?
+            // TODO: Use allExistingKeys directly instead of copying (as non-readonly)? Likely, IHasLocalizableKeysFlatFile will return a new HashSet.
+            // Operating on the passed instance directly could lower best-case allocations to 1, and keep worst case to 2.
+            HashSet<string> keysLeft = new HashSet<string>(allExistingKeys);
+
+            try
+            {
+                // I find this generally less readable, but here indentation gets really bad otherwise.
+                using (FileStream tempFileStream = File.Open(tempFilePath, FileMode.CreateNew, FileAccess.Write))
+                using (StreamWriter streamWriter = new StreamWriter(tempFileStream))
+                using (FileStream originalFileStream = File.Open(filePath, FileMode.OpenOrCreate, FileAccess.Read))
+                using (StreamReader streamReader = new StreamReader(originalFileStream))
+                {
+                    string? columnNames = streamReader.ReadLine();
+
+                    if (string.IsNullOrWhiteSpace(columnNames))
+                        columnNames = string.Join(separator, ColumnNames);
+
+                    streamWriter.WriteLine(columnNames);
+
+                    while (!streamReader.EndOfStream)
+                    {
+                        // Returns null only if the stream end is reached, but we check for that in the loop already.
+                        string currentLine = streamReader.ReadLine()!;
+
+                        int indexOfFirstSeparator = currentLine.IndexOf(separator);
+
+                        // There's no key (e.g. ".something") || the line is just a white space.
+                        if (indexOfFirstSeparator == 0 || string.IsNullOrWhiteSpace(currentLine))
+                            continue;
+
+                        // There's no way to check if a string exists using a span.
+                        string currentKey;
+
+                        // The line only contains a key (if the key still exists in the collection).
+                        if (indexOfFirstSeparator == -1)
+                            currentKey = currentLine;
+                        else
+                            currentKey = currentLine.AsSpan().Slice(0, indexOfFirstSeparator).ToString();
+
+                        // If the key is not "deprecated", copy the whole line in the new file.
+                        // If it is, drop the whole line.
+                        if (keysLeft.Remove(currentKey))
+                            streamWriter.WriteLine(currentLine);
+                    }
+
+                    // Create a new line for each new key not in the original file.
+                    foreach (string leftoverKey in keysLeft)
+                    {
+                        streamWriter.WriteLine(leftoverKey);
+                    }
+                }
+
+                File.Move(tempFilePath, filePath, true);
+            }
+            catch
+            {
+                File.Delete(tempFilePath);
+
+                // Surface any exceptions thrown during merging.
+                throw;
+            }
+        }
+    }
+}
