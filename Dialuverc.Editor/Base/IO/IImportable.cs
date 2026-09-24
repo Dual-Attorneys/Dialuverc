00defd6ea418af2192526a38d4c1b9603e8e4e18
Clancì <60186351+Clancini@users.noreply.github.com>
2026-09-24 13:31:54 +0200

    Make IImportable's summary explicitly explain why it implies IExportable

--- Dialuverc.Editor/Base/IO/IImportable.cs
+++ Dialuverc.Editor/Base/IO/IImportable.cs
@@ -1,10 +1,10 @@
 namespace Dialuverc.Editor.Base.IO
 {
     /// <summary>
-    /// Represents an object which can be imported (i.e. deserialized).
+    /// Represents an object which can be imported (i.e. deserialized).<br/>
+    /// Implies <see cref="IExportable"/> as the object has to be exported at least once for there to be something to import.
     /// </summary>
     // Note: Intended usage of this *does not* include importing arbitrary files. As a consequence, path used is dependent on IExportable.
-    // Being "importable" implies being exported at least once before.
     public interface IImportable : IExportable
     {
         public void DeserializeForImport(Stream stream);
